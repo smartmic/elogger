3c57f7322cd92c4cbb1a798dab67b238d003253b
martin <martin>
2015-05-16 20:21:25 +0000

    own function for buffer read, buffer static variable, void class function for initial timestamp, voltage, current and pf

--- elogger.cpp
+++ elogger.cpp
@@ -3,11 +3,10 @@
 #include <iostream>
 #include <sstream>
 #include <cstring>
+#include <stdlib.h>
 #include <ctime>
 
 #define N 18
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
 
 using namespace std;
  
@@ -19,21 +18,25 @@ template <class T>
     return ss.str();
 }
 
+
+
 class Measurement {
     private:
+        static char* buffer;
         time_t t;
         struct tm ts;
         char timestamp[N];
-        double voltage;
-        double current;
-        double cosphi;
+        float voltage;
+        float current;
+        float cosphi;
     public:
         Measurement();
         ~Measurement();
-        int firstStamp(char* buffer);
-        int getVoltage(char* buffer);
-        int getCurrent(char* buffer);
-        int getPF(char* buffer);
+        int getBuffer(char* filename);
+        int firstStamp(void);
+        int getVoltage(void);
+        int getCurrent(void);
+        int getPF(void);
         void saveEntry(void);
 };
 
@@ -43,7 +46,7 @@ Measurement::Measurement() {
 Measurement::~Measurement() {
 }
 
-int Measurement::firstStamp(char* buffer) {
+int Measurement::firstStamp(void) {
     int offset = 3;
     int datecode;
     memset(&ts, 0, sizeof(tm));
@@ -79,15 +82,12 @@ int Measurement::firstStamp(char* buffer) {
         cout << timestamp;
     else {
         cerr << "strftime failed." <<endl;
-        return EXIT_FAILURE;
+        exit (EXIT_FAILURE);
     }
-    getVoltage(buffer);
-
-
     return EXIT_SUCCESS;
 }
 
-int Measurement::getVoltage(char* buffer) {
+int Measurement::getVoltage(void) {
     int i;
     unsigned int voltcode[2] = {0,0};
     for (i=0;i<=1;i++) {
@@ -95,12 +95,18 @@ int Measurement::getVoltage(char* buffer) {
         buffer++;
     }
     unsigned int volt = ( voltcode[0] << 8 | voltcode[1] );
-    cout << " " << (float)volt/10 ; 
-    getCurrent(buffer);
+    voltage = (float)volt/10;
+
+    if (voltage > 108.0 && voltage < 252.0) 
+        cout << " " << voltage; 
+    else {
+        cerr << "\ninvalid voltage at " << hex << &buffer << endl;
+        exit (EXIT_FAILURE);
+    }
     return EXIT_SUCCESS;
 }
 
-int Measurement::getCurrent(char* buffer) {
+int Measurement::getCurrent(void) {
     int i;
     unsigned int currcode[2] = {0,0};
     for (i=0;i<=1;i++) {
@@ -108,35 +114,40 @@ int Measurement::getCurrent(char* buffer) {
         buffer++;
     }
     unsigned int curr = ( currcode[0] << 8 | currcode[1] );
-    cout << " " << (float)curr/1000;
-    getPF(buffer);
+    current = (float)curr/1000; 
+    
+    if (current >= 0.0)
+        cout << " " << current;
+    else {
+        cerr << "\ninvalid current at " << hex << &buffer << endl;
+        exit (EXIT_FAILURE);
+    }
     return EXIT_SUCCESS;
 }
 
-int Measurement::getPF(char* buffer) {
+int Measurement::getPF(void) {
     unsigned int pf = 0;
     pf = static_cast<int>(*buffer);
     buffer++;
-    cout << " " << (float)pf/100;
+    cosphi = (float)pf/100;
+    if (cosphi >= 0.0 && cosphi <= 1.0)
+        cout << " " << cosphi;
+    else {
+        cerr << "\ninvalid power factor at " << hex << &buffer << endl;
+        exit (EXIT_FAILURE);
+    }
     return EXIT_SUCCESS;
 }
 
 void Measurement::saveEntry(void) {
 }
 
-int main(int argc, char* argv[]) {
-    
-    char* filename[sizeof(&argv[1])];
-    *filename = argv[1];
-    
-    //char logfile[] = "file.log";
-    
+int Measurement::getBuffer(char* filename) {
     ifstream inputfile;
     ifstream::pos_type size;
     // alternative: int size with typecast (int)
-    char* buffer;
 
-    inputfile.open(*filename,ios::in | ios::binary | ios::ate);
+    inputfile.open(filename,ios::in | ios::binary | ios::ate);
     if (inputfile.good()) {
         cout << "inputfile exists." << endl;
         size = inputfile.tellg();
@@ -144,13 +155,30 @@ int main(int argc, char* argv[]) {
         inputfile.seekg (0, ios::beg);
         inputfile.read (buffer, size);
         inputfile.close();
-
-        Measurement entry0;
-        entry0.firstStamp(buffer);
-        delete []buffer;
+        return EXIT_SUCCESS;
     }
-    else
-        cout << "inputfile does not exist!" << endl;
+    else {
+        cerr << "reading file failed." <<endl;
+        exit (EXIT_FAILURE);
+    }
+}
+
+char* Measurement::buffer; 
+
+int main(int argc, char* argv[]) {
+    
+    char* filename[sizeof(&argv[1])];
+    *filename = argv[1];
+    
+    //char logfile[] = "file.log";
+        
+    Measurement entry0;
+    entry0.getBuffer(*filename);
+    entry0.firstStamp();
+    entry0.getVoltage();
+    entry0.getCurrent();
+    entry0.getPF();
+
     return EXIT_SUCCESS;
 }
 
